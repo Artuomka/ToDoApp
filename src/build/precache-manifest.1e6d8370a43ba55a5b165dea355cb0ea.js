0721cc63f1fcab43657fc87c833bb66d96d24e15
Artuom <r.tem_negreyev@meta.ua>
2019-08-30 00:22:01 +0300

    frontend fixes

--- src/build/precache-manifest.1e6d8370a43ba55a5b165dea355cb0ea.js
+++ src/build/precache-manifest.1e6d8370a43ba55a5b165dea355cb0ea.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f7a543a4dabfd67690a816c98af9ad42",
+    "revision": "b673b16782ee01d32d981c72859d77bc",
     "url": "/index.html"
   },
   {
-    "revision": "4f1ce2bb1b65f8bc75eb",
+    "revision": "4dc8dd5ae6d858f0a3f0",
     "url": "/static/css/main.b45d9c38.chunk.css"
   },
   {
-    "revision": "87765bdcf919ed7c4fcf",
-    "url": "/static/js/2.63587c84.chunk.js"
+    "revision": "fed05a194b96f436bd4d",
+    "url": "/static/js/2.46305129.chunk.js"
   },
   {
-    "revision": "4f1ce2bb1b65f8bc75eb",
-    "url": "/static/js/main.81f17a33.chunk.js"
+    "revision": "4dc8dd5ae6d858f0a3f0",
+    "url": "/static/js/main.a3dd1935.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
